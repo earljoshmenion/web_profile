1041cb073926174d0d78b8b0028e771e24958cbb
Earl Josh <menionearl22@gmail.com>
2026-07-19 15:46:06 +0800

    Update profile routing and layout

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section>
+      <h1>Welcome</h1>
+      <p>This is your home page.</p>
+    </section>
+  );
+}
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,13 +1,18 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
-import MainLayout from "./Layout/MainLayout";
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home";
 
-const root = document.getElementById("root");
+const Route = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MainLayout />} />
-    </Routes>
-  </BrowserRouter>,
-);
+export default Route;
